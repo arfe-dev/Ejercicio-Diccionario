cae495ac0d98555549b3561332440c74b2a32edd
LORENZO CREO KOCHER <50049697@ort>
2026-03-26 09:05:59 -0300

    Empezando el menu

--- Program.cs
+++ Program.cs
@@ -4,6 +4,52 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        
+      
+        do
+            {
+                Console.Clear();
+                menu.mostrarMenu(list + "CURSO DEL COLEGIO");
+
+                opcion = menu.pedirOpcion(list.Count);
+
+                switch (opcion)
+                {
+                    case 1:
+                        agregarAlumno(curso);
+                        break;
+
+                    case 2:
+                        buscarAlumno(curso);
+                        break;
+
+                    case 3:
+                        agregarFalta(curso);
+                        break;
+
+                    case 4:
+                        mostrarAlumnos(curso);
+                        break;
+
+                    case 5:
+                        alumnosLibres(curso);
+                        break;
+
+                    case 6:
+                        Console.WriteLine("Saliendo...");
+                        break;
+
+                    default:
+                        Console.WriteLine("Opción inválida");
+                        break;
+
+
+
+                }
+                Console.WriteLine("\nPresione una tecla para continuar...");
+                Console.ReadKey();
+            } while (opcion != list.Count);
+
+      
     }
 }
